3110010282b07f3fa1f9ae22ef40e29617ab6b53
Olivier Mengué <olivier@blueboard.io>
2018-06-06 06:52:15 +0200

    Improve Map
    - expose internals to ease iteration
    - add Parse method

--- map.go
+++ map.go
@@ -3,21 +3,26 @@ package jsonweb
 import (
 	"errors"
 
+	"github.com/dolmen-go/jsonptr"
 	"github.com/dolmen-go/jsonptrerror"
 )
 
 // Map stores a group of Extractor by names.
-type Map struct {
-	m map[string]*Extractor
-}
+type Map map[string]*Extractor
 
 func (m *Map) UnmarshalJSON(b []byte) error {
-	err := jsonptrerror.Unmarshal(b, &m.m)
+	var nude map[string]*Extractor
+	err := jsonptrerror.Unmarshal(b, &nude)
 	if err != nil {
 		return err
 	}
-	if m.m == nil {
+	if nude == nil {
 		return errors.New("/: empty webmap")
 	}
+	*m = nude
 	return nil
+}
+
+func (m Map) Parse(key string, doc interface{}, visit ContextVisitor) error {
+	return m[key].Parse((*Context)(nil), jsonptr.Pointer(nil), doc, visit)
 }
